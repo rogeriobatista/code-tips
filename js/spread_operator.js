b19765d40ccf5cb1b0757179e6807951d0e291bd
Rogerio Batista <rogeriobatista@Rogerios-MacBook-Pro.local>
2020-03-16 23:09:24 -0300

    Sperad Operator Uses

--- js/spread_operator.js
+++ js/spread_operator.js
@@ -1,4 +1,22 @@
-// Spread Operator
-function sum(...params) {
-    return params.reduce((sum, param) => sum += param, 0)
-}
+
+// Uses to Spread Operator
+
+// Get Sum
+const sum = (...values) => values.reduce((sum, value) => sum += value, 0)
+sum(1, 2, 3, 4, 5)
+
+// Get Min Value
+const min = (...values) => Math.min(...values)
+min(5, 4, 3, 2, 1)
+
+// Get Max Value
+const max = (...values) => Math.max(...values)
+max(10, 20, 30 ,40, 50)
+
+// Concat arrays
+const mergeValues = (arr, arr2) => [...arr,...arr2]
+mergeValues([1,2,3], [4,5,6,7,8])
+
+// Copy an array without keep references
+const copyValues = (arr) => [...arr]
+copyValues(['a', 'b', 'c'])
